e2ced3a8cd43cb2f9f31ef09f28a1ed8ec9b11e4
sosohime <theziming@126.com>
2018-02-01 17:40:27 +0800

    改了个按钮的文案

--- common/component/adminButtonGroup.js
+++ common/component/adminButtonGroup.js
@@ -25,7 +25,7 @@ const buttonGroup = (adminProps) => {
         <div className={styles.adminBtnGroup}>
             <h4>权限狗你好，开始吧！</h4>
             <AddCalendar {...addProps}></AddCalendar>
-            <Button onClick={showAds}>广告管理(没做)</Button>
+            <Button onClick={showAds}>广告管理</Button>
             <UpdateTJTime {...updateTJTimeProps}></UpdateTJTime>
             <InvitationUser></InvitationUser>
         </div>
